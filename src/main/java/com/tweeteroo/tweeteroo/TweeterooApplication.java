e692f851b7a8b57f75b3ca91d31951810f05af6c
seu nome <adnanbezerra@gmail.com>
2023-01-16 18:09:26 -0300

    start project

---
+++ src/main/java/com/tweeteroo/tweeteroo/TweeterooApplication.java
@@ -0,0 +1,13 @@
+package com.tweeteroo.tweeteroo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweeterooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweeterooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tweeteroo/tweeteroo/TweeterooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tweeteroo.tweeteroo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweeterooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
