7db62604958abcf1c3bd23b917dbdf3ef362fc9b
seu nome <adnanbezerra@gmail.com>
2023-01-16 20:31:27 -0300

    feat: create basic architecture

---
+++ src/main/java/com/tweeteroo/tweeteroo/controllers/UserController.java
@@ -0,0 +1,9 @@
+package com.tweeteroo.tweeteroo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sign-up")
+public class UserController {
+}
